d7a24eed8db056c1010d61d3988be3da8b9fa7ad
Miras Khaval <miskafrajdej@gmail.com>
2024-12-25 13:12:37 +0500

    Create a base class named Person with the following attributes:

--- src/task1.java
+++ src/task1.java
@@ -1,5 +1,5 @@
 
-public class assignment1 {
+public class task1 {
     public static void main(String[] args) {
         Person1 person1 = new Person1("Miras", "Khaval", 17, true);
         System.out.println(person1.toString());
