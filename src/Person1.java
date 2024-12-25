d57e44fa165bdd9fb6a9fcf4652ab66aa3ab359d
Miras Khaval <miskafrajdej@gmail.com>
2024-12-25 12:55:37 +0500

    Create a base class named Person with the following attributes

---
+++ src/Person1.java
@@ -0,0 +1,23 @@
+public class Person1 {
+
+     String name;
+     String surname;
+     int age;
+     boolean gender;
+
+
+    public Person1(String name, String surname, int age, boolean gender) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.gender = gender;
+
+
+    }
+
+    @Override
+    public String toString() {
+        String genderStr = gender ? "Male" : "Female";
+        return "Hi, I am " + name + " " + surname + ", a " + age + "-year-old " + genderStr + ".";
+    }
+}
--- src/assignment1.java
+++ src/assignment1.java
@@ -1,6 +1,7 @@
-public class assignment1 {
-     public static void main(String[] args) {
-         System.out.printf("");
 
+public class assignment1 {
+    public static void main(String[] args) {
+        Person1 person1 = new Person1("Miras", "Khaval", 17, true);
+        System.out.println(person1.toString());
     }
 }
