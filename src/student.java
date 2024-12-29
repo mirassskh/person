384e8fc27166ceedf600968faade32b320ec93d5
Miras Khaval <miskafrajdej@gmail.com>
2024-12-29 23:49:34 +0500

    Create two derived classes Student and Teacher that inherit from Person.

---
+++ src/student.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class student extends Person {
+    private int studentID;
+    private List<Integer> grades;
+
+    public student(String name, String surname, int age, boolean gender, int studentID) {
+        super(name, surname, age, gender);
+        this.studentID = studentID;
+        this.grades = new ArrayList<>();
+    }
+
+    public void addGrade(int grade) {
+        grades.add(grade);
+    }
+
+    public double calculateGPA() {
+        int total = 0;
+        for (int grade : grades) {
+            total += grade;
+        }
+        return grades.size() > 0 ? (double) total / grades.size() : 0;
+    }
+
+    @Override
+    public String toString() {
+        String gradesString = grades.isEmpty() ? "No grades" : grades.toString();
+        return super.toString() + " I am a student with ID " + studentID  + gradesString + ".";
+    }
+}
+class Teacher extends Person {
+    private String subject;
+    private int yearsOfExperience;
+    private int salary;
+
+    public Teacher(String name, String surname, int age, boolean gender, String subject, int yearsOfExperience, int salary) {
+        super(name, surname, age, gender);
+        this.subject = subject;
+        this.yearsOfExperience = yearsOfExperience;
+        this.salary = salary;
+    }
+
+    public void giveRaise(double percentage) {
+        if (yearsOfExperience > 10) {
+            salary += salary * percentage / 100;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " I teach " + subject + " "+ yearsOfExperience + " "+salary;
+    }
+}
